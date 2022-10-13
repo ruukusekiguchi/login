42686edb5eb33ebfe9b05306ef0b7d8adff0d473
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2022-10-13 16:45:04 +0900

    作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="new">
+      <p>ユーザ名</p>
+      <input type="text" v-model="item" v-on:keydown.enter="onKeyDown">
+      <p>パスワード</p>
+      <input type="text" v-model="pass" v-on:keydown.enter="onKeyDown">
+      <button @click="newSend">登録</button>
+    </div>
+
+    <div id="login">
+      <p>ユーザ名</p>
+      <input type="text" v-on:keydown.enter="onKeyDown">
+      <p>パスワード</p>
+      <input type="text" v-on:keydown.enter="onKeyDown">
+      <button @click="loginSend">ログイン</button>
+    </div>
+    <p>{{ user.newitem }}</p>
+    <p>{{ welcome }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      item: "",
+      pass: "",
+      users: [],
+    };
+  },
+  methods: {
+    onKeyDown() {
+      this.additem();
+    },
+    newSend(){
+      if(this.item == "" || this.pass == ""){
+        alert("未入力はやめてくださいよ");
+        return;
+      }
+      for(i = 0; i< this.newid;i++){
+        count++;
+      }
+      var user = {
+        newid: this.count+1,
+        newitem: this.item,
+        newpass: this.pass,
+      };
+      this.users.push(user);
+      // this.item = "";
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
